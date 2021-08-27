e8b12c81101e7cd18e6cbe4ee5196ba9e4ab9273
abhishekwick <87739667+abhishekwick@users.noreply.github.com>
2021-08-27 12:06:26 +1000

    Added water plant function
    27/8/2021
    added water plant function
    week 7 homework.

--- ishp/ishp.ino
+++ ishp/ishp.ino
@@ -1,124 +1,3 @@
-//
-///***************************************************
-//  Adafruit invests time and resources providing this open source code,
-//  please support Adafruit and open-source hardware by purchasing
-//  products from Adafruit!
-//
-//  Written by Limor Fried/Ladyada for Adafruit Industries.
-//  MIT license, all text above must be included in any redistribution
-// ****************************************************/
-//
-//// RTC
-//#include "RTClib.h"
-//
-//RTC_PCF8523 rtc;
-//char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-//
-//// EINK
-//#include "Adafruit_ThinkInk.h"
-//
-//#define EPD_CS      15
-//#define EPD_DC      33
-//#define SRAM_CS     32
-//#define EPD_RESET   -1 // can set to -1 and share with microcontroller Reset!
-//#define EPD_BUSY    -1 // can set to -1 to not use a pin (will wait a fixed delay)
-//
-//// 2.13" Monochrome displays with 250x122 pixels and SSD1675 chipset
-//ThinkInk_213_Mono_B72 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
-//
-////soil mosisture
-//int moisturevalue = 0; //value for storing moisture value 
-//int soilPin = 12;//Variable for Soil moisture Power
-//
-//void setup() {
-//  Serial.begin(9600);
-//  while (!Serial) {
-//    delay(10);
-//  }
-// setupSD();
-//  // RTC
-//  if (! rtc.begin()) {
-//    Serial.println("Couldn't find RTC");
-//    Serial.flush();
-//    abort();
-//  }
-//
-//  // The following line can be uncommented if the time needs to be reset.
-//  //  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-//
-//  rtc.start();
-//
-//  //EINK
-//  display.begin(THINKINK_MONO);
-//  display.clearBuffer();
-//}
-//
-//void loop() {
-//
-//  // Gets the current date and time, and writes it to the Eink display.
-//  String currentTime = getDateTimeAsString();
-//
-//  drawText("The Current Time and\nDate is", EPD_BLACK, 2, 0, 0);
-//
-//  // writes the current time on the bottom half of the display (y is height)
-//  drawText(currentTime, EPD_BLACK, 2, 0, 75);
-//
-//  // Draws a line from the leftmost pixel, on line 50, to the rightmost pixel (250) on line 50.
-//  display.drawLine(0, 50, 250, 50, EPD_BLACK);
-// 
-//  
-//  int moisture = readSoil();
-//   drawText(String(moisture), EPD_BLACK, 2, 0, 100);
-//   display.display();
-//  // waits 180 seconds (3 minutes) as per guidelines from adafruit.
-//  delay(180000);
-//  display.clearBuffer();
-//}
-//
-//void drawText(String text, uint16_t color, int textSize, int x, int y) {
-//  display.setCursor(x, y);
-//  display.setTextColor(color);
-//  display.setTextSize(textSize);
-//  display.setTextWrap(true);
-//  display.print(text);
-//
-//}
-//
-//String getDateTimeAsString() {
-//  DateTime now = rtc.now();
-//
-//  //Prints the date and time to the Serial monitor for debugging.
-//  /*
-//    Serial.print(now.year(), DEC);
-//    Serial.print('/');
-//    Serial.print(now.month(), DEC);
-//    Serial.print('/');
-//    Serial.print(now.day(), DEC);
-//    Serial.print(" (");
-//    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-//    Serial.print(") ");
-//    Serial.print(now.hour(), DEC);
-//    Serial.print(':');
-//    Serial.print(now.minute(), DEC);
-//    Serial.print(':');
-//    Serial.print(now.second(), DEC);
-//    Serial.println();
-//  */
-//
-//  // Converts the date and time into a human-readable format.
-//  char humanReadableDate[20];
-//  sprintf(humanReadableDate, "%02d:%02d:%02d %02d/%02d/%02d",  now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
-//
-//  return humanReadableDate;
-//}
-////This is a function used to get the soil moisture content
-//int readSoil()
-//{
-//    moisturevalue = analogRead(soilPin);//Read the SIG value form sensor 
-//    return moisturevalue;//send current moisture value
-//}
-
-
 /***************************************************
   Adafruit invests time and resources providing this open source code,
   please support Adafruit and open-source hardware by purchasing
@@ -164,6 +43,7 @@ void setup() {
   while (!Serial) {
     delay(10);
   }
+  delay(1000);
 
   // SD Card
   setupSD();
@@ -176,7 +56,7 @@ void setup() {
   }
 
   // The following line can be uncommented if the time needs to be reset.
-  //  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+//  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
   rtc.start();
 
@@ -201,9 +81,15 @@ void loop() {
 
   // Draws a line from the leftmost pixel, on line 50, to the rightmost pixel (250) on line 50.
   display.drawLine(0, 50, 250, 50, EPD_BLACK);
+
+
+  int moisture = readSoil();
+  drawText(String(moisture), EPD_BLACK, 2, 0, 100);
   display.display();
 
-
+waterPlant(moisture);
+  
+  logEvent("Updating the EPD");
   // waits 180 seconds (3 minutes) as per guidelines from adafruit.
   delay(180000);
   display.clearBuffer();
@@ -247,7 +133,7 @@ String getDateTimeAsString() {
 
 
 void setupSD() {
-  if (!SD.begin()) {
+  if (!SD.begin(33)) {
     Serial.println("Card Mount Failed");
     return;
   }
@@ -258,6 +144,7 @@ void setupSD() {
     return;
   }
   Serial.println("SD Started");
+//  delay(1000);
 }
 
 void logEvent(String dataToLog) {
@@ -313,4 +200,20 @@ int readSoil()
 {
   moistureValue = analogRead(soilPin);//Read the SIG value form sensor
   return moistureValue;//send current moisture value
+}
+
+void waterPlant(int moistureValue) {
+/*  
+ *   **Write a function** which takes the moisture value, 
+ *   and if it's below a certain value, turns the pump on. 
+ *   The function is to be called `waterPlant()` which will 
+ *   take the moisture value as an argument, and return no value.
+ */
+ if (int moistureValue < 1000 ){
+  //motor/pump on
+  myMoter->run(Forward);
+ } else {
+  //motor/pump off
+  myMotor->run(RELEASE);
+ }
 }
